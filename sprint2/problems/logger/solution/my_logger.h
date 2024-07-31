e7f23218d4ae43a1ced0980f708b7d730a5385c0
Alex Rud <lexa.roach@gmail.com>
2024-07-31 20:17:30 +0300

    fix_2

--- sprint2/problems/logger/solution/my_logger.h
+++ sprint2/problems/logger/solution/my_logger.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <iostream>
+
 #include <chrono>
 #include <iomanip>
 #include <fstream>
@@ -9,100 +9,98 @@
 #include <optional>
 #include <mutex>
 #include <thread>
+#include <syncstream>
 
 using namespace std::literals;
 
 #define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
 
-template <typename T, typename = typename std::enable_if<!std::is_same<T, std::string>::value, typename T::iterator>::type>
-std::ofstream& operator << (std::ofstream& os, T const& t)
-{
-    for (auto it = std::begin(t); it != std::end(t); it++) {
-        os << *it;
-    }
-    return os;
-}
-
-class Logger
-{
-    std::chrono::system_clock::time_point GetTime() const
-    {
-        if (manual_ts_)
+class Logger {
+    auto GetTime() const {
         {
-            return *manual_ts_;
+            std::lock_guard g(lock_time_);
+            if (manual_ts_) {
+                return *manual_ts_;
+            }
         }
-
         return std::chrono::system_clock::now();
     }
 
-    auto GetTimeStamp() const
-    {
-        const auto now = GetTime();
+    auto GetTimeStamp(const std::chrono::_V2::system_clock::time_point now) const {
+        // const auto now = GetTime();
         const auto t_c = std::chrono::system_clock::to_time_t(now);
         return std::put_time(std::localtime(&t_c), "%F %T");
     }
 
     // Для имени файла возьмите дату с форматом "%Y_%m_%d"
-    std::string GetFileTimeStamp() const
-    {
-        const auto t_c = std::chrono::system_clock::to_time_t(GetTime());
+    std::string GetFileTimeStamp(const std::chrono::_V2::system_clock::time_point now) const {
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
         std::stringstream ss;
         ss << std::put_time(std::localtime(&t_c), "%Y_%m_%d");
         return ss.str();
+    }
+
+    void CheckNameLogFile(const std::chrono::_V2::system_clock::time_point now) {
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        int day_of_month = std::localtime(&t_c)->tm_mday;
+        if (day_of_file_ != day_of_month) {
+            day_of_file_ = day_of_month;
+            std::string file_name = "/var/log/sample_log_"s + GetFileTimeStamp(now) + ".log"s;
+            log_file_.close();
+            log_file_.open(file_name); //, std::ios::app
+            if (!log_file_) {
+                throw std::runtime_error("File not open!"s);
+            }
+        }
     }
 
     Logger() = default;
     Logger(const Logger&) = delete;
 
 public:
-    static Logger& GetInstance()
-    {
+    static Logger& GetInstance() {
         static Logger obj;
         return obj;
     }
 
-    template <typename T>
-    int log_helper(T&& t)
-    {
-        this->file_log_ << t;
-        return 0;
+    template<typename T0, class... Ts>
+    void LogImpl(std::ostream& out, const T0& v0, const Ts&... args) {
+        using namespace std::literals;
+        out << v0;
+        (..., (out << args)); //  << " "sv
     }
 
     // Выведите в поток все аргументы.
-    template <typename... Ts>
-    void Log(const Ts &...args)
-    {
-        auto new_file_log_name_ = "/var/log/sample_log_"s + GetFileTimeStamp() + ".log";
+    template<class... Ts>
+    void Log(const Ts&... args) {
+        const auto now = GetTime();
 
-        std::lock_guard lk{ mutex_ };
 
-        if (new_file_log_name_ != file_log_name_)
-        {
-            if (file_log_.is_open())
-                file_log_.close();
-
-            file_log_.open(new_file_log_name_, std::ios::out);
-            file_log_name_ = new_file_log_name_;
+        std::stringstream strm;
+        if constexpr (sizeof...(args) != 0) {
+            LogImpl(strm, args...);
         }
-
-
-        file_log_ << GetTimeStamp() << ": ";
-        auto y = { log_helper(args)... };
-        file_log_ << "\n";
+        std::lock_guard g(lock_file_);
+        CheckNameLogFile(now);
+        // std::osyncstream sync_stream(log_file_);
+        log_file_ << GetTimeStamp(now) << ": "sv << strm.str() << std::endl;
     }
 
+
     // Установите manual_ts_. Учтите, что эта операция может выполняться
-    // параллельно с выводом в поток, вам нужно предусмотреть
+    // параллельно с выводом в поток, вам нужно предусмотреть 
     // синхронизацию.
-    void SetTimestamp(std::chrono::system_clock::time_point ts)
-    {
-        std::lock_guard lk{ mutex_ };
-        manual_ts_ = std::move(ts);
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard g(lock_time_);
+        manual_ts_ = ts;
     }
 
 private:
     std::optional<std::chrono::system_clock::time_point> manual_ts_;
-    std::mutex mutex_;
-    std::string file_log_name_;
-    std::ofstream file_log_;
+    // для демонстрации пока оставим файл в текущей директории
+    int day_of_file_ = 0;
+    std::ofstream log_file_;
+
+    mutable std::mutex lock_time_;
+    mutable std::mutex lock_file_;
 };
